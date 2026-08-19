0b30f2a2e5d3d3805efe069d9b8897053d2a36ba
Aavash Shakya <aavashrajshakya@gmail.com>
2026-08-19 10:42:30 +0545

    Add loops

--- Shopping_Cart/Shopping _Cart.py
+++ Shopping_Cart/Shopping _Cart.py
@@ -1,33 +1,46 @@
+print("======Welcome to the Aavash Mart=====\n")
+print("Here is your cart 🛒\n")
 cart=[]
-one=(input("Enter your first item to the  cart:"))
-cart.append(one)
-two=(input("Enter your secound item to the cart:"))
-cart.append(two)
-three=(input("Enter your third item to the cart:"))
-cart.append(three)
-four=(input("Enter your forth item to the cart:"))
-cart.append(four)
-five=(input("Enter your fifth item to the cart:"))
-cart.append(five)
-print(f"Your cart list:{cart}")
-print(f"YOUR first item:{cart[0]}")
-print(f"Your last item{cart[-1]}")
-print(f"your number of items:{len(cart)}")
-if(input(f'Do you want to add one more item(y/n)'))=='y':
-    add_item=input("Enter the item you want to add:")
-    cart.append(add_item)
-    print(f"item{add_item}addded successfully:")
-    print(f"Your updated cart items are:{cart}")
-else:
-    print(f"Your final cart items are:{cart}")
-if(input("Do you want to remove anthing from the cart(y/n):"))=='y':
-    rmv=input("which item do you want to remove:")
-    cart.remove(rmv)
-    print(f"Your updated cart is:{cart}")
-    print("=====Item removed sucessfully======")
-    print(f"your final cart items are{cart}")
-    print("Thank you for shopping with us")    
-else:
-    print("Thank you for shopping with us")
-print("================================")
-print("HEllO world!")
+while True:
+    ch=input("DO you want to add items in the cart(y/n):")
+    ch=ch.lower()
+    if(ch=='y'):
+        item=input("\nEnter the name of item you want to add:")
+        cart.append(item)
+        print(f"=={item} Added Sucessfully===\n")
+        print(f"Your cart contains{cart}\n")
+        continue
+    elif(ch=='n'):
+        break
+    else:
+        print("Invalid choice,Enter either(y/n)")
+        continue 
+while True:
+    if(len(cart)>0):
+        ch=input("Do you want to remove anthing from the cart(y/n):")
+        ch=ch.lower()
+        if(ch=='y'):
+            rmv=input("\nWhich item do you want to remove:")
+            if (cart.count(rmv)>0):
+                cart.remove(rmv)
+                print(f"====={rmv} removed sucessfully======\n")
+                print(f"Your updated cart is:{cart}")
+                continue
+            else:
+                print("There is no such items in the cart!\n")
+                print(f"Plz check the cart again and select what you want to remove:\n{cart}\n")
+                continue
+        elif(ch=='n'):
+            print(f"Your final cart contains:{cart}\n")
+            print(f"YOUR first item:{cart[0]}")
+            print(f"Your last item:{cart[-1]}")
+            print(f"Your number of items:{len(cart)}\n")
+            break
+        else:
+            print("Invalid choice! Enter either(y/n)")
+            continue
+    else:
+        break
+print("\n\n====Thank you for shopping with us======\n")
+            
+            
