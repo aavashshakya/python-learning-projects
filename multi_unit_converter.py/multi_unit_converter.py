c4510e6aa9f2db549207dc6ac9801c1fd5d44b1f
Aavash Shakya <aavashrajshakya@gmail.com>
2026-08-21 09:28:55 +0545

    added new loops

--- multi_unit_converter.py/multi_unit_converter.py
+++ multi_unit_converter.py/multi_unit_converter.py
@@ -38,83 +38,110 @@ def kg_lb(num):
 def lb_kg(num):
     num=float(num/2.20462)
     return num 
-
-print("=====Welcome to unit conveter======\n")
-print("Please select the unit you want to convert:")
-ch=int(input('\n1.Length\n2.Temperature\n3.Mass\nchoice:'))
-if(ch==1):
-    print("Please select the option below:")
-    option=int(input("""\n1.km ➡️   m  
+while True : #loop 1
+    
+    print("=====Welcome to unit conveter======\n")
+    print("Please select the unit you want to convert:")
+    while True: #loop 2
+        ch=int(input('\n1.Length\n2.Temperature\n3.Mass\nchoice:'))
+        if(ch==1):
+            print("Please select the option below:")
+            option=int(input("""\n1.km ➡️   m  
 2.m  ➡️   km
 3.m  ➡️   cm
 4.cm ➡️   m
 choice:"""))
-    num=float(input("Enter the lenght you want to convert:"))
-    if(option==1):
-        result=multiply_1000(num)
-        print(f'{result:.2f} m')
-        
-    elif(option==2):
-        result=divide_1000(num)
-        print(f"{result:.2f} Km")
-        
-    elif(option==3):
-        result=m_cm(num)
-        print(f"{result:.2f} cm")
-        
-    else:
-        result=cm_m(num)
-        print(f"{result:.2f} m")
 
-elif(ch==2):
-    num=float(input("Enter the Temperature you want to convert:"))
-    print("Please select the option below:")
-    option=int(input("""\n1.c  ➡️   f    
+            num=float(input("Enter the lenght you want to convert:"))
+            if(option==1):
+                result=multiply_1000(num)
+                print(f'{result:.2f} m')
+                break #break loop 1
+                
+            elif(option==2):
+                result=divide_1000(num)
+                print(f"{result:.2f} Km")
+                break
+                
+            elif(option==3):
+                result=m_cm(num)
+                print(f"{result:.2f} cm")
+                break
+                
+            else:
+                result=cm_m(num)
+                print(f"{result:.2f} m")
+                break
+
+        elif(ch==2):
+            num=float(input("Enter the Temperature you want to convert:"))
+            print("Please select the option below:")
+            option=int(input("""\n1.c  ➡️   f    
 2.f  ➡️   c
 3.c  ➡️   k
 4.k  ➡️   c
 choice:"""))
-    if(option==1):
-        result=c_f(num)
-        print(f"{result:.2f} F")
-        
-    elif(option==2):
-        result=f_c(num)
-        print(f"{result:.2f} C")
-        
-    elif(option==3):
-        result=c_k(num)
-        print(f"{result:.2f} K")
-        
-    else:
-        result=k_c(num)
-        print(f"{result:.2f} C")
+            if(option==1):
+                result=c_f(num)
+                print(f"{result:.2f} F")
+                break
+                
+            elif(option==2):
+                result=f_c(num)
+                print(f"{result:.2f} C")
+                break
+                
+            elif(option==3):
+                result=c_k(num)
+                print(f"{result:.2f} K")
+                break
+                
+            else:
+                result=k_c(num)
+                print(f"{result:.2f} C")
+                break
 
-elif(ch==3):
-     num=float(input("Enter the Mass you want to convert:"))
-     print("Please select the option below:")
-     option=int(input("""\n1.kg ➡️   g   
+        elif(ch==3):
+            num=float(input("Enter the Mass you want to convert:"))
+            print("Please select the option below:")
+            option=int(input("""\n1.kg ➡️   g   
 2.g  ➡️   kg
 3.kg ➡️   lb
 4.lb ➡️   kg
 choice:"""))
-     
-     if(option==1):
-         result=multiply_1000(num)
-         print(f"{result:.2f} kg")
-         
-     elif(option==2):
-         result=divide_1000(num)
-         print(f"{result:.2f} km")
-         
-     elif(option==3):
-         result= kg_lb(num)
-         print(f"{result:.2f} lb")
-         
-     else:
-         result=lb_kg(num)
-         print(f"{result:.2f}")
+            
+            if(option==1):
+                result=multiply_1000(num)
+                print(f"{result:.2f} kg")
+                break
+                
+            elif(option==2):
+                result=divide_1000(num)
+                print(f"{result:.2f} km")
+                break
+                
+            elif(option==3):
+                result= kg_lb(num)
+                print(f"{result:.2f} lb")
+                break
+                
+            else:
+                result=lb_kg(num)
+                print(f"{result:.2f}")
+                break
 
 
-else:
-    print("Enter a vaild number from the option above!")
+        else:
+            print("Enter a vaild number from the option above!(1/2/3)\n")
+            break
+          
+    again=input("\nDo you want to convert again?(y/n):")
+    
+    if(again=='y'):
+        continue
+    elif(again=='n'):
+        break
+    else:
+        print("!Enter a vaild choice(y/n)")
+        
+print("\n====Thank you======")
